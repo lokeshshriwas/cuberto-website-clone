9bf1c1894d25f372735e9fe64ca8db9c09ad5478
lokeshshriwas <99093170+lokeshshriwas@users.noreply.github.com>
2023-10-27 15:58:11 +0530

    file: script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+Shery.mouseFollower();
+Shery.makeMagnet(".magnet")
+Shery.hoverWithMediaCircle(".hrv", {videos: ["./0.mp4", "./2.mp4", "./3.mp4"]})
+
+gsap.to(".leftelem",{
+    y: "-300%",
+    ease: Power2,
+    scrollTrigger:{
+        trigger: ".fimages",
+        pin: true,
+        scroller: "body",
+        start: "top top",
+        end: "bottom bottom",
+        endTrigger: ".last",
+        scrub: 1,}
+})
+
+let sections = document.querySelectorAll(".leftelem")
+Shery.imageEffect(".images", {
+    style: 5,
+    config: {onMouse: {value: 1}},
+    slideStyle: (setScroll) => {
+        sections.forEach(function(section, index){
+            ScrollTrigger.create({
+                trigger: section,
+                start: "top top",
+                scrub: 1,
+                onUpdate: function(prog){
+                    setScroll(prog.progress + index)
+                }
+            })
+        })
+    },
+  });
